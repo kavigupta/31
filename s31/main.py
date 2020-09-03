c610c3950b43cf0a181ae4f6626150b14a38cd26
Kavi Gupta <kavi@berkeley.edu>
2020-09-02 23:23:29 -0700

    some reorganization

--- s31/main.py
+++ s31/main.py
@@ -7,7 +7,9 @@ from .notify import notify
 
 def main():
     parser = argparse.ArgumentParser("31")
-    parser.add_argument("-c", "--command", help="The command to run")
+    group = parser.add_mutually_exclusive_group(required=True)
+
+    group.add_argument("-c", "--command", help="The command to run")
     parser.add_argument(
         "-s",
         "--sync",
@@ -17,10 +19,11 @@ def main():
     parser.add_argument(
         "-n", "--screen-name", help="The name of the screen session to create"
     )
-    parser.add_argument(
+    group.add_argument(
         "--config",
-        nargs="+",
-        help="Pass in configuration arguments to set in the form --config key1 value1 key2 value2 ...",
+        nargs=2,
+        metavar=("key", "value"),
+        help="Edit a configuration argument",
     )
     args = parser.parse_args()
 
@@ -31,20 +34,8 @@ def main():
 
 
 def run_command(args):
-    if (args.command is not None) == (args.config is not None):
-        raise RuntimeError(
-            "Must provide exactly one of (1) a command to run via -c or (2) configuration setup via --config"
-        )
-
     if args.config is not None:
-        if len(args.config) % 2 != 0:
-            raise RuntimeError(
-                "Unmatched key/value pair in configuration: " + args.config[-1]
-            )
-        new_config = {
-            args.config[i]: args.config[i + 1] for i in range(0, len(args.config), 2)
-        }
-        update_config(new_config)
+        update_config({args.config[0] : args.config[1]})
         return
 
     config = Config()
