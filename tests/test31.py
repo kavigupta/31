c26dcbdf74d1bb92deca3c9aed77a60c8031338b
Kavi Gupta <kavi@berkeley.edu>
2020-09-05 14:17:18 -0700

    handle \r\n correctly

--- tests/test31.py
+++ tests/test31.py
@@ -9,4 +9,4 @@ RC_TEST = os.path.join(os.path.dirname(__file__), "testrc.json")
 class Test31(unittest.TestCase):
     def assertOutput(self, command, output):
         actual = subprocess.check_output([*command, "--config-file", RC_TEST])
-        self.assertEqual(actual.decode("utf-8").split("\n"), output)
+        self.assertEqual(actual.decode("utf-8").replace("\r\n", "\n").split("\n"), output)
